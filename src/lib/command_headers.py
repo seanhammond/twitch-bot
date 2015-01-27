7a09a046c30ce0eeb69b479de1077cf7e38b9d2e
seanhammond <mylifefornerzhul@gmail.com>
2015-01-27 15:48:26 -0800

    first commit

--- src/lib/command_headers.py
+++ src/lib/command_headers.py
@@ -16,6 +16,12 @@ commands = {
 		'limit': 30,
 		'argc': 3,
 		'return': 'command'
+	},
+
+	'!jesus': {
+		'limit': 0,
+		'argc': 0,
+		'return': 'John 3:16 - For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life'
 	}
 }
 
